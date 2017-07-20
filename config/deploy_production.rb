71ebce2f500c22eb56c54c9c6dad72abc2a2ffbd
Fareed Dudhia <fareed@populi.io>
2017-07-21 00:57:15 +0100

    Add mina config

---
+++ config/deploy_production.rb
@@ -0,0 +1,70 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+require 'mina/puma'
+require_relative 'mina_systemd'
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :rails_env, 'production'
+set :domain, '52.90.168.81'
+set :repository, "git@github.com:bestmadeco/shopify_integration.git"
+set :branch, 'master'
+set :rbenv_path, "/opt/rbenv"
+set :deploy_to, "/home/deploy/apps/shopify_integration_#{rails_env}"
+#set :identity_file, "~/.ssh/bestmade.pem"
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['tmp/pids', 'log','config/puma.rb']
+# Optional settings:
+set :user, 'deploy'    # Username in the server to SSH to.
+
+#set :term_mode, nil
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+  invoke :'rbenv:load'
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    #invoke :'sidekiq:quiet'
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    to :launch do
+      #Puma
+      invoke :'puma:phased_restart'
+      invoke :"systemctl:restart['globol_#{rails_env}']"
+      invoke :"systemctl:restart['nginx']"
+      #Sidekiq
+      #invoke :'sidekiq:restart'
+    end
+  end
+end
---
+++ config/mina_systemd.rb
@@ -0,0 +1,21 @@
+namespace :systemctl do
+  desc "Start a systemd service"
+  task :start, [:service] do |t, args|
+    command %[sudo systemctl start #{args[:service]}]
+  end
+
+  desc "Restart a systemd service"
+  task :restart, [:service] do |t, args|
+    command %[sudo systemctl restart #{args[:service]}]
+  end
+
+  desc "Stop a systemd service"
+  task :stop, [:service] do |t, args|
+    command %[sudo systemctl stop #{args[:service]}]
+  end
+
+  desc "Get status of a systemd service"
+  task :status, [:service] do |t, args|
+    command %[sudo systemctl status #{args[:service]}]
+  end
+end
